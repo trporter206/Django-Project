3a2daf4a9192a514c63e091abdc1409955b3d751
trporter <torri.porter@yahoo.com>
2019-03-20 18:29:49 -0700

    index basics done

--- barback/tests.py
+++ barback/tests.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+from .models import Cocktail
+import datetime
 
-# Create your tests here.
+class CocktailModelTests(TestCase):
+    def test_was_published_recently_with_future_q(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_cocktail = Cocktail(pub_date=time)
+        self.assertIs(future_cocktail.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_cocktail(self):
+        time = timezone.now() - datetime.timedelta(days=2, seconds=1)
+        old_cocktail = Cocktail(pub_date=time)
+        self.assertIs(old_cocktail.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_cocktail(self):
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_cocktail = Cocktail(pub_date=time)
+        self.assertIs(recent_cocktail.was_published_recently(), True)
+
+def create_cocktail(cocktail_name, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Cocktail.objects.create(cocktail_name=cocktail_name, pub_date=time)
+
+class CocktailIndexViewTests(TestCase):
+    def test_no_cocktails(self):
+        response = self.client.get(reverse('barback:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No cocktails are available.")
+        self.assertQuerysetEqual(response.context['latest_cocktail_list'], [])
+
+    def test_past_cocktail(self):
+        create_cocktail(cocktail_name="Past cocktail.", days=-30)
+        response = self.client.get(reverse('barback:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_cocktail_list'],
+            ['<Cocktail: Past cocktail.>']
+        )
+
+    def test_future_cocktail(self):
+        create_cocktail(cocktail_name="Future cocktail.", days=30)
+        response = self.client.get(reverse('barback:index'))
+        self.assertContains(response, "No cocktails are available.")
+        self.assertQuerysetEqual(response.context['latest_cocktail_list'], [])
+
+    def test_future_cocktail_and_past_cocktail(self):
+        create_cocktail(cocktail_name="Past cocktail.", days=-30)
+        create_cocktail(cocktail_name="Future cocktail.", days=30)
+        response = self.client.get(reverse('barback:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_cocktail_list'],
+            ['<Cocktail: Past cocktail.>']
+        )
+
+    def test_two_past_cocktails(self):
+        create_cocktail(cocktail_name="Past cocktail 1.", days=-30)
+        create_cocktail(cocktail_name="Past cocktail 2.", days=-5)
+        response = self.client.get(reverse('barback:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_cocktail_list'],
+            ['<Cocktail: Past cocktail 2.>', '<Cocktail: Past cocktail 1.>']
+        )
--- barback/views.py
+++ barback/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from django.utils import timezone
+from .models import Cocktail
 
-def index(request):
-    return HttpResponse("Hello world. This is barback index")
+class IndexView(generic.ListView):
+    template_name = 'barback/index.html'
+    context_object_name = 'latest_cocktail_list'
+
+    def get_queryset(self):
+        return Cocktail.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
